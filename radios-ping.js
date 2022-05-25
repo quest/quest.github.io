5a6bab973e1ea96332704803436969dae6e4f6e4
Victor San Martin <victor@eclass.com>
2022-05-24 23:51:32 -0400

    nuevas radios

---
+++ radios-ping.js
@@ -0,0 +1,33 @@
+const https = require("https")
+const http = require("http")
+
+let radios = require("./api/radios/radios.json")
+let count = {
+    "http": 0,
+    "https": 0
+}
+radios.radios.forEach(function (radio) {
+    if (radio.play.urlStream.substring(0, 5) == 'https') {
+        count.https++
+
+        https.get(radio.play.urlStream, function(res) {
+            if (res.statusCode != 200) {
+                console.log({
+                    "id": radio.id,
+                    "stream": radio.play.urlStream,
+                    "statusCode": res.statusCode
+                })
+            }
+        }).on('error', function(e) {
+            console.log({
+                "id": radio.id,
+                "stream": radio.play.urlStream,
+                "error": e
+            })
+        });
+    }
+    else {
+        count.http++
+    }
+})
+console.log(count)
